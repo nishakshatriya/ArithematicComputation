2744521d9be6efc52780a5d2b4c1228a279a61c8
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-07 17:03:56 +0530

    Computed Inputs

--- ArithmeticOperations.sh
+++ ArithmeticOperations.sh
@@ -1,5 +1,8 @@
 #!/bin/bash/ -x
 
-read -p "Enter the input one" a
-read -p "Enter the input two" b
-read -p "Enter the input three" c
+read -p "Enter the input a -->" a
+read -p "Enter the input b -->" b
+read -p "Enter the input c -->" c
+
+result=$(( $a + $b * $c ))
+echo $result
