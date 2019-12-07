80901b75466dca21df372939a02816e35e3ba3f9
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-07 17:25:23 +0530

    ComputedInput5]

--- ArithmeticOperations.sh
+++ ArithmeticOperations.sh
@@ -7,7 +7,9 @@ read -p "Enter the input c -->" c
 result=$(( $a + $b * $c ))
 result2=$(( $a * $b + $c ))
 result3=$(( $c + $a / $b ))
+result4=$(($a % $b + $c ))
+
 echo "result1: $result"
 echo "result2: $result2"
 echo "result3: $result3"
-
+echo "result4: $result4"
