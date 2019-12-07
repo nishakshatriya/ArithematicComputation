5d9604b451617d569b4185dce039f4372850ec22
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-07 17:41:07 +0530

    Added: ComputedResult to Dictonary

--- ArithmeticOperations.sh
+++ ArithmeticOperations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash/ -x
-
+declare -A dict
 read -p "Enter the input a -->" a
 read -p "Enter the input b -->" b
 read -p "Enter the input c -->" c
@@ -8,6 +8,11 @@ result=$(( $a + $b * $c ))
 result2=$(( $a * $b + $c ))
 result3=$(( $c + $a / $b ))
 result4=$(($a % $b + $c ))
+
+dict["result 1"]=$result
+dict["result 2"]=$result2
+dict["result 3"]=$result3
+dict["result 4"]=$result4
 
 echo "result1: $result"
 echo "result2: $result2"
