80e71c71cbfffa5c041b0d74b41e8578003da7e8
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-09 09:53:07 +0530

    Added: Sorted Result in Descending

--- ArithmeticOperations.sh
+++ ArithmeticOperations.sh
@@ -6,29 +6,28 @@ read -p "Enter the input b -->" b
 read -p "Enter the input c -->" c
 
 result=$(( $a + $b * $c ))
+echo $result
 result2=$(( $a * $b + $c ))
+echo $result2
 result3=$(( $c + $a / $b ))
+echo $result3
 result4=$(($a % $b + $c ))
+echo $result4
 
-dict["result 1"]=$result
-dict["result 2"]=$result2
-dict["result 3"]=$result3
-dict["result 4"]=$result4
+dict["result1"]=$result
+dict["result2"]=$result2
+dict["result3"]=$result3
+dict["result4"]=$result4
 
-echo "result1: $result"
-echo "result2: $result2"
-echo "result3: $result3"
-echo "result4: $result4"
-
-for(( i=0; i<4; i++ ))
+for(( i=0; i<=4; i++ ))
 do
-	array[$i]="${dict[$i]}"
+	array[$i]="${dict[result$i]}"
 done
+
 echo ${dict[@]}
 
-for d in ${array[@]}
+for acs in ${!array[@]}
 do
-	echo ${array[$d]}
-done | sort -nr 
-
+	echo ${array[$acs]}
+done | sort -nr
 
